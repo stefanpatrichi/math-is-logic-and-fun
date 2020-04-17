a0e011975974e634ea8100a35be2f896e4cfc2f1
stefanpatrichi <stefanpatrichi07@gmail.com>
2020-04-17 09:26:45 +0300

    add program loop

--- src/pi_approx.cpp
+++ src/pi_approx.cpp
@@ -8,12 +8,16 @@ double aleatoriu() {
 int main() {
   long long total, in_cerc, i;
   double x, y;
-  cout << "Nr. puncte: "; cin >> total;
-  in_cerc = 0;
-  for (i = 0; i < total; i++) {
-    x = aleatoriu(); y = aleatoriu();
-    if(sqrt(x*x + y*y) <= 1.0) in_cerc++;
+  char rasp = 'y';
+  while(rasp == 'y') {
+    cout << "Nr. puncte: "; cin >> total;
+    in_cerc = 0;
+    for (i = 0; i < total; i++) {
+      x = aleatoriu(); y = aleatoriu();
+      if(sqrt(x*x + y*y) <= 1.0) in_cerc++;
+    }
+    cout << 4.0 * in_cerc / total << "\nDoriti sa continuati? (y/n)";
+    cin >> rasp;
   }
-  cout << 4.0 * in_cerc / total << '\n';
   return 0;
 }
